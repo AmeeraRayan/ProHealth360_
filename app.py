6f17546fd6135466abba6b21e6358b582f263a46
AmeeraRayan <142424119+AmeeraRayan@users.noreply.github.com>
2023-10-15 19:16:53 +0300

    covidchanges

--- app.py
+++ app.py
@@ -62,12 +62,6 @@ def faqs():
 def prevention():
    return render_template('prevention.html')
 
-@app.route('/upload_chest.html')
-def upload_chest():
-   return render_template('upload_chest.html')
-@app.route('/upload_ct.html')
-def upload_ct():
-   return render_template('upload_ct.html')
 
 
 
@@ -129,7 +123,19 @@ def home():
 def covid():
     return render_template('covid.html')
 
+@app.route('/upload.html')
+def uploadCovid():
+   return render_template('upload.html') 
 
+@app.route('/upload_chest.html')
+def upload_chest():
+   return render_template('upload_chest.html')
+
+
+@app.route('/upload_ct.html')
+def upload_ct():
+   return render_template('upload_ct.html')
+ 
 @app.route('/breastcancer')
 def breast_cancer():
     return render_template('breastcancer.html')
@@ -189,6 +195,142 @@ def resultc():
         else:
             flash('Allowed image types are - png, jpg, jpeg')
             return redirect(request.url)
+
+######covid
+@app.route('/uploaded_chest', methods = ['POST', 'GET'])
+def uploaded_chest():
+   if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            # filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'upload_chest.jpg'))
+
+   resnet_chest = load_model('jupyterNotebooksCOVID/resnet_chest.h5')
+   vgg_chest = load_model('jupyterNotebooksCOVID/vgg_chest.h5')
+   inception_chest = load_model('jupyterNotebooksCOVID/inceptionv3_chest.h5')
+   xception_chest = load_model('jupyterNotebooksCOVID/xception_chest.h5')
+
+   image = cv2.imread('./static/uploads/assets/images/upload_chest.jpg') # read file 
+   image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # arrange format as per keras
+   image = cv2.resize(image,(224,224))
+   image = np.array(image) / 255
+   image = np.expand_dims(image, axis=0)
+   
+   resnet_pred = resnet_chest.predict(image)
+   probability = resnet_pred[0]
+   print("Resnet Predictions:")
+   if probability[0] > 0.5:
+      resnet_chest_pred = str('%.2f' % (probability[0]*100) + '% COVID') 
+   else:
+      resnet_chest_pred = str('%.2f' % ((1-probability[0])*100) + '% NonCOVID')
+   print(resnet_chest_pred)
+
+   vgg_pred = vgg_chest.predict(image)
+   probability = vgg_pred[0]
+   print("VGG Predictions:")
+   if probability[0] > 0.5:
+      vgg_chest_pred = str('%.2f' % (probability[0]*100) + '% COVID') 
+   else:
+      vgg_chest_pred = str('%.2f' % ((1-probability[0])*100) + '% NonCOVID')
+   print(vgg_chest_pred)
+
+   inception_pred = inception_chest.predict(image)
+   probability = inception_pred[0]
+   print("Inception Predictions:")
+   if probability[0] > 0.5:
+      inception_chest_pred = str('%.2f' % (probability[0]*100) + '% COVID') 
+   else:
+      inception_chest_pred = str('%.2f' % ((1-probability[0])*100) + '% NonCOVID')
+   print(inception_chest_pred)
+
+   xception_pred = xception_chest.predict(image)
+   probability = xception_pred[0]
+   print("Xception Predictions:")
+   if probability[0] > 0.5:
+      xception_chest_pred = str('%.2f' % (probability[0]*100) + '% COVID') 
+   else:
+      xception_chest_pred = str('%.2f' % ((1-probability[0])*100) + '% NonCOVID')
+   print(xception_chest_pred)
+
+   return render_template('results_chest.html',resnet_chest_pred=resnet_chest_pred,vgg_chest_pred=vgg_chest_pred,inception_chest_pred=inception_chest_pred,xception_chest_pred=xception_chest_pred)
+##ct
+@app.route('/uploaded_ct', methods = ['POST', 'GET'])
+def uploaded_ct():
+   if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            # filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'upload_ct.jpg'))
+
+   resnet_ct = load_model('jupyterNotebooksCOVID/resnet_ct.h5')
+   vgg_ct = load_model('jupyterNotebooksCOVID/vgg_ct.h5')
+   inception_ct = load_model('jupyterNotebooksCOVID/inception_ct.h5')
+   xception_ct = load_model('jupyterNotebooksCOVID/xception_ct.h5')
+
+   image = cv2.imread('./static/uploads/assets/images/upload_ct.jpg') # read file 
+   image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # arrange format as per keras
+   image = cv2.resize(image,(224,224))
+   image = np.array(image) / 255
+   image = np.expand_dims(image, axis=0)
+   
+   resnet_pred = resnet_ct.predict(image)
+   probability = resnet_pred[0]
+   print("Resnet Predictions:")
+   if probability[0] > 0.5:
+      resnet_ct_pred = str('%.2f' % (probability[0]*100) + '% COVID') 
+   else:
+      resnet_ct_pred = str('%.2f' % ((1-probability[0])*100) + '% NonCOVID')
+   print(resnet_ct_pred)
+
+   vgg_pred = vgg_ct.predict(image)
+   probability = vgg_pred[0]
+   print("VGG Predictions:")
+   if probability[0] > 0.5:
+      vgg_ct_pred = str('%.2f' % (probability[0]*100) + '% COVID') 
+   else:
+      vgg_ct_pred = str('%.2f' % ((1-probability[0])*100) + '% NonCOVID')
+   print(vgg_ct_pred)
+
+   inception_pred = inception_ct.predict(image)
+   probability = inception_pred[0]
+   print("Inception Predictions:")
+   if probability[0] > 0.5:
+      inception_ct_pred = str('%.2f' % (probability[0]*100) + '% COVID') 
+   else:
+      inception_ct_pred = str('%.2f' % ((1-probability[0])*100) + '% NonCOVID')
+   print(inception_ct_pred)
+
+   xception_pred = xception_ct.predict(image)
+   probability = xception_pred[0]
+   print("Xception Predictions:")
+   if probability[0] > 0.5:
+      xception_ct_pred = str('%.2f' % (probability[0]*100) + '% COVID') 
+   else:
+      xception_ct_pred = str('%.2f' % ((1-probability[0])*100) + '% NonCOVID')
+   print(xception_ct_pred)
+
+   return render_template('results_ct.html',resnet_ct_pred=resnet_ct_pred,vgg_ct_pred=vgg_ct_pred,inception_ct_pred=inception_ct_pred,xception_ct_pred=xception_ct_pred)
+
+
+
 ####### brain #########################
 def predict_Btumor(img_path):
     model_load = load_model("Trained ModelBrain/brain_tumor.h5")
