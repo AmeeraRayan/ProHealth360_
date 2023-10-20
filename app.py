6ca9c106052544eb67791321f5605f1ad6ee68fe
khaldyAseel <132208072+khaldyAseel@users.noreply.github.com>
2023-10-20 22:43:28 +0300

    aa

--- app.py
+++ app.py
@@ -283,11 +283,9 @@ def predict_cancer():
 
     # Apply the sigmoid function to get probabilities
     probabilities = sigmoid(raw_scores)
-    print(probabilities.shape)  # Print the shape of probabilities
-    print(probabilities)  # Print the content of probabilities
 
     # The probability for "Breast cancer" can be obtained based on the class index
-    probability_of_breast_cancer = probabilities[0][1] * 100  # Probability for class 1 (malignant)
+    probability_of_breast_cancer = probabilities[0] * 100  # Probability for class 1 (malignant)
 
     return render_template('cancer_detection.html', probability=probability_of_breast_cancer)
 
